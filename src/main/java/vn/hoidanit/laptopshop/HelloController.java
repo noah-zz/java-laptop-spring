b951e780a93b9319ba4fc6e2f76b7c38344a51da
Nguyen Mau Nam <nguyenmaunamit@gmail.com>
2024-06-24 18:10:32 +0700

    spring-mvc-p7

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -9,4 +9,14 @@ public class HelloController {
     public String index() {
         return "Hello Nam!";
     }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin  access this page!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user this access this page!";
+    }
 }
